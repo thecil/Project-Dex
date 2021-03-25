7bb80347d60dcf56db274526487565db1ce84fae
thecil <53628989+thecil@users.noreply.github.com>
2021-03-25 14:30:41 -0400

    marketOrder

--- solidity/contracts/Dex.sol
+++ solidity/contracts/Dex.sol
@@ -19,6 +19,7 @@ contract Dex is Wallet{
         bytes32 ticker;
         uint256 amount;
         uint256 price;
+        uint256 filled;
     }
     uint256 public nextOrderId = 0;
 
@@ -37,10 +38,10 @@ contract Dex is Wallet{
         else if(_orderType == Side.SELL){
             require(balances[msg.sender][_ticker] >= _amount, "createLimitOrder: Not enough Token Balance deposited");
         }
-
+        
         Order[] storage orders = orderBook[_ticker][uint256(_orderType)];
         orders.push(
-            Order(nextOrderId, msg.sender, _orderType, _ticker, _amount, _price)
+            Order(nextOrderId, msg.sender, _orderType, _ticker, _amount, _price, 0)
         );
 
         //Bubble sort'
@@ -74,10 +75,71 @@ contract Dex is Wallet{
 
     function createMarketOrder(Side _orderType, bytes32 _ticker, uint256 _amount) public{
         if(_orderType == Side.BUY){
+            // Verify that the trader(buyer) has enough ETH to cover the purchase
             require(balances[msg.sender][bytes32("ETH")] >= _amount, "createMarketOrder: Not enough ETH Balance deposited");
         }
         else if(_orderType == Side.SELL){
             require(balances[msg.sender][_ticker] >= _amount, "createMarketOrder: Not enough Token Balance deposited");
+        }
+
+        uint256 orderBookSide;
+
+        if(_orderType == Side.BUY){
+            orderBookSide = 1;
+        }
+        else if(_orderType == Side.SELL){
+            orderBookSide = 0;
+        }
+        Order[] storage orders = orderBook[_ticker][uint256(orderBookSide)];
+
+        uint256 _totalFilled = 0;
+
+        for (uint256 _i = 0; _i < orders.length && _totalFilled < _amount; _i++) {
+            // How much we can fill from order[i]
+            uint256 _leftToFill = _amount.sub(_totalFilled);    
+            uint256 _availableToFill = orders[_i].amount.sub(orders[_i].filled);
+            uint256 _filled = 0;
+            if(_availableToFill > _leftToFill){
+                _filled = _leftToFill; // fill the entire market order
+            }
+            else{
+                _filled = _availableToFill; // fill as much as available in order[_i]
+            }
+            // Update _totalFilled
+            _totalFilled = _totalFilled.add(_filled);     
+            // Update orders.filled + amount  
+            orders[_i].filled = orders[_i].filled.add(_filled); 
+            uint256 _cost = _filled.mul(orders[_i].price);
+
+            if(_orderType == Side.BUY){
+                // Verify that the trader(buyer) has enough ETH to cover the purchase
+                require(balances[msg.sender][bytes32("ETH")] >= _cost, "createMarketOrder: _cost: Not enough ETH Balance deposited");
+                // Execute the trade, msg.sender is buyer
+                // buyer receive tokens, transfer eth to seller
+                balances[msg.sender][_ticker] = balances[msg.sender][_ticker].add(_filled);
+                balances[msg.sender][bytes32("ETH")] = balances[msg.sender][bytes32("ETH")].sub(_cost);  
+                // transfer tokens from seller to buyer, receive eth from buyer
+                balances[orders[_i].trader][_ticker] = balances[orders[_i].trader][_ticker].sub(_filled);
+                balances[orders[_i].trader][bytes32("ETH")] = balances[orders[_i].trader][bytes32("ETH")].add(_cost);             
+            }
+            else if(_orderType == Side.SELL){
+                // Execute the trade, msg.sender is seller  
+                // transfer tokens from seller to buyer, receive eth from buyer
+                balances[msg.sender][_ticker] = balances[msg.sender][_ticker].sub(_filled);
+                balances[msg.sender][bytes32("ETH")] = balances[msg.sender][bytes32("ETH")].add(_cost);
+                // seller receive tokens, transfer eth to buyer
+                balances[orders[_i].trader][_ticker] = balances[orders[_i].trader][_ticker].add(_filled);
+                balances[orders[_i].trader][bytes32("ETH")] = balances[orders[_i].trader][bytes32("ETH")].sub(_cost);               
+            }
+        }
+            // loop through the orderbook and remove 100% filled orders
+        while(orders.length > 0 && orders[0].filled == orders[0].amount){
+            // remove the top elem,ent in the orders array by orverwriting every element
+            // whit the next element in the order list
+            for (uint256 _i = 0; _i < orders.length; _i++) {
+                orders[_i] = orders[_i + 1];
+            }
+            orders.pop();
         }
     }
 }
--- solidity/test/4_MarketOrders_test.js
+++ solidity/test/4_MarketOrders_test.js
@@ -19,10 +19,11 @@ const {
     expectRevert, // Assertions for transactions that should fail
   } = require('@openzeppelin/test-helpers');
   
-//track balance
-const balance = require('@openzeppelin/test-helpers/src/balance');
+//test-helper tools
+const balances = require('@openzeppelin/test-helpers/src/balance');
 const { web3 } = require('@openzeppelin/test-helpers/src/setup');
 const { assertion } = require('@openzeppelin/test-helpers/src/expectRevert');
+const ether = require('@openzeppelin/test-helpers/src/ether');
 
 // Main function that is executed during the test
 contract("DEX Market Orders", ([owner, alfa, beta, charlie]) => {
@@ -31,9 +32,7 @@ contract("DEX Market Orders", ([owner, alfa, beta, charlie]) => {
     let daiInstance;
     let dexInstance;
     let _ticker;
-    var _qtyEth = function(_amount) {
-        return (web3.utils.toWei(_amount.toString(), "ether"))
-    }
+
     async function _symbol(_tokenInstance){
         let _res = await _tokenInstance.symbol();
         _res = web3.utils.fromUtf8(_res);
@@ -43,73 +42,81 @@ contract("DEX Market Orders", ([owner, alfa, beta, charlie]) => {
     //set contracts instances
     before(async function() {
         // Deploy tokens to testnet
-        linkInstance = await ERC20.link.new(_qtyEth(1000));
-        daiInstance = await ERC20.dai.new(_qtyEth(1000));
+        linkInstance = await ERC20.link.new(ether("1000"));
+        daiInstance = await ERC20.dai.new(ether("1000"));
         dexInstance = await Dex.new();
         _ticker = await _symbol(linkInstance);
     });
 
     describe("Market Orders", () => {
         it("1. Creating a SELL market order, the seller needs to have enough tokens for the trade", async function (){ 
-            let balance = await dexInstance.balances(alfa, web3.utils.fromUtf8("ETH"));
+            let balance = await dexInstance.balances(owner, web3.utils.fromUtf8("ETH"));
             assert.equal(new BN(balance), 0, "Initial ETH balance is not 0");
             
             await expectRevert(
-                dexInstance.createMarketOrder(orderType.sell, _ticker, _qtyEth(1), {from: owner}),
+                dexInstance.createMarketOrder(orderType.sell, _ticker, ether("1"), {from: owner}),
                 "createMarketOrder: Not enough Token Balance deposited"
             );
         }); 
         it("2. Creating a BUY market order, the buyer needs to have enough ETH for the trade", async function (){
             await expectRevert(
-                dexInstance.createMarketOrder(orderType.buy, _ticker, _qtyEth(1), {from: owner}),
+                dexInstance.createMarketOrder(orderType.buy, _ticker, ether("1"), {from: owner}),
                 "createMarketOrder: Not enough ETH Balance deposited"
             );
         });
-        // 
         it("3. Market orders can be submitted even if the order book is empty", async function (){
             const buyOrderBook = await dexInstance.getOrderBook(_ticker, orderType.buy);
             const sellOrderBook = await dexInstance.getOrderBook(_ticker, orderType.sell);
             assert(buyOrderBook.length == 0 && sellOrderBook.length == 0, "OrderBooks should be empty at the start");
 
-            await dexInstance.depositEth({value: _qtyEth(1), from: owner});
-            await dexInstance.createMarketOrder(orderType.buy, _ticker, _qtyEth(1), {from: owner});
+            await dexInstance.depositEth({value: ether("5"), from: owner});
+            await dexInstance.createMarketOrder(orderType.buy, _ticker, ether("1"), {from: owner});
         });
-        /*
-        it("4. Market orders should be filled until the order books is empty or the market order is 100% filled", async function (){
-            const buyOrderBook = await dexInstance.getOrderBook(_ticker, orderType.buy);
-            const sellOrderBook = await dexInstance.getOrderBook(_ticker, orderType.sell);
+        
+        it("4. Market orders should not fill more limit orders than the market order amount ", async function (){
+            // check orderbooks are empty
+            buyOrderBook = await dexInstance.getOrderBook(_ticker, orderType.buy);
+            sellOrderBook = await dexInstance.getOrderBook(_ticker, orderType.sell);
             assert(buyOrderBook.length == 0 && sellOrderBook.length == 0, "OrderBooks should be empty at the start");
-
+            // add link token to dex
             await dexInstance.addToken( _ticker, linkInstance.address);
-            //sent link tokens to accounts (from owner)
-            await linkInstance.transfer(alfa, _qtyEth(1), {from:owner});
-            await linkInstance.transfer(beta, _qtyEth(1), {from:owner});
-            await linkInstance.transfer(charlie, _qtyEth(1), {from:owner});
-            
-            //approve dex to spend link on account behalf
-            await linkInstance.approve(dexInstance.address, _qtyEth(5), {from: owner});
-            await linkInstance.approve(dexInstance.address, _qtyEth(5), {from: alfa});
-            await linkInstance.approve(dexInstance.address, _qtyEth(5), {from: beta});
-            await linkInstance.approve(dexInstance.address, _qtyEth(5), {from: charlie});
-
-            //depost Link into dex, all accounts
-            await dexInstance.depositToken(_qtyEth(1) , _ticker ,{from: owner});
-            await dexInstance.depositToken(_qtyEth(1) , _ticker ,{from: alfa});
-            await dexInstance.depositToken(_qtyEth(1) , _ticker ,{from: beta});
-            await dexInstance.depositToken(_qtyEth(1) , _ticker ,{from: charlie});
+            // sent link tokens to accounts (from owner)
+            await linkInstance.transfer(alfa, ether("1"), {from:owner});
+            await linkInstance.transfer(beta, ether("1"), {from:owner});
+            await linkInstance.transfer(charlie, ether("1"), {from:owner});          
+            // approve dex to spend link on account behalf
+            await linkInstance.approve(dexInstance.address, ether("2"), {from: owner});
+            await linkInstance.approve(dexInstance.address, ether("2"), {from: alfa});
+            await linkInstance.approve(dexInstance.address, ether("2"), {from: beta});
+            await linkInstance.approve(dexInstance.address, ether("2"), {from: charlie});
+            // depost Link into dex, all accounts
+            await dexInstance.depositToken(ether("1") , _ticker ,{from: owner});
+            await dexInstance.depositToken(ether("1") , _ticker ,{from: alfa});
+            await dexInstance.depositToken(ether("1") , _ticker ,{from: beta});
+            await dexInstance.depositToken(ether("1") , _ticker ,{from: charlie});
             // Fill up the sell order book
-            await dexInstance.createLimitOrder(orderType.buy, _ticker, _qtyEth(1), _qtyEth(0.2), {from: alfa});
-            await dexInstance.createLimitOrder(orderType.buy, _ticker, _qtyEth(1), _qtyEth(0.5), {from: beta});
-            await dexInstance.createLimitOrder(orderType.buy, _ticker, _qtyEth(1), _qtyEth(1), {from: charlei});
-            //depositEth and create market order to buy 2/3 links
-            await dexInstance.depositEth({value: _qtyEth(1), from: owner});
-            await dexInstance.createMarketOrder(orderType.buy, _ticker, _qtyEth(2), {from: owner});
+            await dexInstance.createLimitOrder(orderType.sell, _ticker, ether("1"), ether("0.25"), {from: alfa});
+            await dexInstance.createLimitOrder(orderType.sell, _ticker, ether("1"), ether("0.5"), {from: beta});
+            await dexInstance.createLimitOrder(orderType.sell, _ticker, ether("1"), ether("1"), {from: charlie});
+            //depositEth and create market order to buy 2/3 links orders
+            // await dexInstance.depositEth({value: ether("2"), from: owner});
 
-            const sellOrderBook = await dexInstance.getOrderBook(_ticker, orderType.sell);
-            assert(sellOrderBook.length == 0, "OrderBooks should be empty at the start");
+            const mapBalance = await dexInstance.balances(owner, web3.utils.fromUtf8("ETH"));
+            console.log(mapBalance.toString());
+            console.log(mapBalance.toString());
+            console.log(mapBalance.toString());
+
+            await dexInstance.createMarketOrder(orderType.buy, _ticker, ether("0.75"), {from: owner});
+            // check sell orderbook
+            sellOrderBook = await dexInstance.getOrderBook(_ticker, orderType.sell);
+            
+
+            assert(sellOrderBook.length == 1, "Sell side orderbook should only have 1 order left");
+            assert(sellOrderBook[0].filled == 0, "Sell side order should have 0 filled");
+            
 
         });
-        */
+        
         // The eth balance of the buyer should decrease with the filled amount
         // The token balances of the sellers should decrease with the filled amounts
         // Filled limit orders should be removed from the orderbook
